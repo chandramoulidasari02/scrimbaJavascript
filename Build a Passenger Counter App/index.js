9eb224f96e91d8bd27cf1e6ca1d933c3ef99f25f
chandramoulidasari02 <dasari.chandu02@gmail.com>
2025-07-21 11:27:04 +0000

    updated

--- Build a Passenger Counter App/index.js
+++ Build a Passenger Counter App/index.js
@@ -1,23 +1,15 @@
-// function increment() {
-//   count = 0
-//   console.log("The button was clicked");
-//   count += 1;
-//   console.log(count);
-// }
-
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
 let countEL = document.getElementById("count-el");
+let saveEL = document.getElementById("save-el");
 
 let count = 0;
 function increment() {
   count += 1;
-  countEL.innerText = count;
+  countEL.textContent = count;
 }
 
 function save() {
-  console.log(count);
+  let saveCount = count + " - ";
+  saveEL.textContent += saveCount;
+  count = 0;
+  countEL.textContent = count;
 }
-
---
+++ Practice/1.js
@@ -0,0 +1,12 @@
+// Create two variables, firstName and lastName
+
+// Concatenate the two variables into a third variable called fullName
+
+// Log fullName to the console
+
+let firstName = "chandu";
+let lastName = "Sai";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
